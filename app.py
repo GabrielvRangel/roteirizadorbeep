c208f52c2ecc3ba8c44ae682c1c1fa4a278ab5b6
Gabriel Rangel <gabrielrangel.001@gmail.com>
2022-09-22 18:58:20 -0300

    teste

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ def roteirizar():
         return render_template("mapa.html")
 
 if __name__ == "__main__":
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
     app.run(debug=True)
 
  
