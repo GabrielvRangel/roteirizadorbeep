9a4014f6e216fda6ec8da75b2192f20d81d4711a
Gabriel Rangel <gabrielrangel.001@gmail.com>
2022-09-22 21:02:52 -0300

    teste

--- servidor.py
+++ servidor.py
@@ -9,5 +9,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/roteirizar",methods=["GET","POST"])
+def roteirizar():
+    data = request.args.get('data')
+    hub = request.args.get('hub')
+    produto = request.args.get('produto')
+    tempo = request.args.get('tempo')   
+    Sessão = model.Roteirização(data, hub, produto, tempo)
+    if not data or not hub or not produto or not tempo:
+        mensagem = 'Favor preencher todos os campos corretamente!'
+        return render_template("index.html", error=mensagem)
+    else:
+        Sessão.Roteirizar()
+        return render_template("mapa.html")
+
 if __name__ == "__main__":
     app.run()
