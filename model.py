239848e825a394edb4c697b87995921d7d746f38
Gabriel Rangel <gabrielrangel.001@gmail.com>
2022-09-22 21:07:45 -0300

    teste

--- model.py
+++ model.py
@@ -1,16 +1,3 @@
-from sqlite3 import Timestamp
-from telnetlib import theNULL
-import pandas as pd
-import psycopg2 as pg
-from sqlalchemy import Time, create_engine
-import googlemaps
-import json
-import datetime
-import folium
-from branca.element import Figure
-from folium.plugins import MarkerCluster
-import folium.plugins as plugins
-import os
 
 class Roteirização:
     print('teste')
